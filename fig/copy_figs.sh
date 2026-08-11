8463738e9530783451c48fa72f262f1bc640dfd9
Mika Mäki <mika.x.maki@helsinki.fi>
2026-08-11 21:41:29 +0300

    Add figure copying script

---
+++ fig/copy_figs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+GIT_DIR="$(dirname "$(dirname "${SCRIPT_DIR}")")"
+PTTOOLS_FIG_DIR="${GIT_DIR}/pttools/examples/fig"
+PTPLOT_FIG_DIR="${GIT_DIR}/PTPlot/examples/fig/svg"
+
+copy_fig() {
+  if [ $# -gt 1 ]; then
+    TARGET="${SCRIPT_DIR}/$2.svg"
+  else
+    TARGET="${SCRIPT_DIR}/$1.svg"
+  fi
+  cp "$1" "${TARGET}"
+}
+
+copy_fig_pttools() {
+  copy_fig "${PTTOOLS_FIG_DIR}/$1.svg" $2
+}
+
+copy_fig_ptplot() {
+  copy_fig "${PTPLOT_FIG_DIR}/$1.svg" $2
+}
+
+copy_fig_pttools "const_cs_gw" "const_cs_gw_v2"
+copy_fig_pttools "const_cs_gw_1" "const_cs_gw_1_v2"
+copy_fig_pttools "const_cs_gw_2" "const_cs_gw_2_v2"
+copy_fig_pttools "const_cs_gw_omgw0_1" "const_cs_gw_omgw0_1_v2"
+copy_fig_pttools "const_cs_gw_omgw0_2" "const_cs_gw_omgw0_2_v2"
+copy_fig_pttools "const_cs_gw_v" "const_cs_gw_v_v2"
+copy_fig_pttools "const_cs_gw_v_1" "const_cs_gw_v_1_v2"
+copy_fig_pttools "const_cs_gw_v_2" "const_cs_gw_v_2_v2"
+
+copy_fig_ptplot "singlet_jonathan_snr_alpha_beta_bpl" "singlet_jonathan_snr_alpha_beta_bpl_v2"
+copy_fig_ptplot "singlet_jonathan_snr_alpha_beta_dbpl" "singlet_jonathan_snr_alpha_beta_dbpl_v2"
+copy_fig_ptplot "singlet_jonathan_snr_alpha_beta_ssm" "singlet_jonathan_snr_alpha_beta_ssm_v2"
+copy_fig_ptplot "singlet_jonathan_snr_histogram" "singlet_jonathan_snr_histogram_v2"
